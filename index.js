0d1d6a051a5f1b3f3d5555763fb40b54f645387a
Jose <J_Lanza.13@live.com>
2021-08-11 13:36:59 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+// Write your code
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2;
 
-// Write your code here
+let random = Math.ceil(Math.random() * 10);
+
+let num3 = 28
+let num4 = 7
+const mod = num3 / num4;
+
+const max = Math.max(5, 10, 15, 20);
